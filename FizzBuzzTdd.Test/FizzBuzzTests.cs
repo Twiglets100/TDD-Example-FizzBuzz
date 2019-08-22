2c5079948e696b80f03780b5ce787a9be2ff749e
Tom French <tomfrench100@gmail.com>
2019-08-22 19:38:55 +0100

    Return fizz if multiple of 3.

--- FizzBuzzTdd.Test/FizzBuzzTests.cs
+++ FizzBuzzTdd.Test/FizzBuzzTests.cs
@@ -20,5 +20,12 @@ namespace FizzBuzzTdd.Test
             // Assert
             Assert.AreEqual(input.ToString(), output);
         }
+
+        [TestCase(3)]
+        public void GetValueIfMultipleOf3ReturnFizz(int input)
+        {
+            var output = FizzBuzz.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
     }
 }
